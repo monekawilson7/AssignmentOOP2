765cb5d6b9b09444e9e872ecbf084f90bf70db27
moneka <monekawilson16@gmail.com>
2025-07-09 18:18:21 +0300

    Add project files.

---
+++ AssignmentOOP2/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOOP2
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public SecurirtyLevel SecurirtyLevel { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime HireDate { get; set; }
+
+        private char gender;
+        public char Gender { 
+        get { return gender; }
+            set {
+                if (value == 'M' || value == 'F')
+                    gender = value;
+                else
+                    throw new ArgumentException("Gender must be either 'M' or 'F'.");
+            }
+        }
+        public override string ToString()
+        {
+            return $"ID: {Id}\n" +
+                $"Name: {Name}\n" +
+                $":Securirty Level: {SecurirtyLevel}\n" +
+                $"Salary: {string.Format("{0:C}", Salary)}\n" +
+                $"Hire Date: {HireDate.ToShortDateString()}\n" +
+                $"Gender: {(Gender == 'M' ? "Male" : "Female")}\n";
+
+        }
+
+    }
+}
---
+++ AssignmentOOP2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AssignmentOOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            try
+            {
+                Employee emp = new Employee();
+                Console.WriteLine("Enter Employee ID:");
+                emp.Id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Employee Name:");
+                emp.Name = Console.ReadLine();
+                Console.WriteLine("Enter Security Level (Guest, Developer, Secretary, DBA):");
+                string secInput = Console.ReadLine();
+                emp.SecurirtyLevel = (SecurirtyLevel)Enum.Parse(typeof(SecurirtyLevel), secInput, true);
+                Console.WriteLine("Enter Salary:");
+                emp.Salary = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Enter Hire Date:");
+                emp.HireDate = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Gender");
+                emp.Gender = Convert.ToChar(Console.ReadLine().ToUpper());
+                Console.WriteLine("Employee Information");
+                Console.WriteLine(emp.ToString());
+            }
+            catch (Exception ex)
+            { 
+            Console.WriteLine(ex.ToString()); 
+            }
+            #endregion
+
+        }
+    }
+}
